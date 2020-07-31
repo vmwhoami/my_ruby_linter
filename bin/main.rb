db717019987db9e2449bb029847bed8f54aeb35f
Vitalie Melnic <vmwhoami@gmail.com>
2020-07-31 23:26:07 +0300

    require_relative linterator from the main file

--- bin/main.rb
+++ bin/main.rb
@@ -1,16 +1,21 @@
 
 #!/usr/bin/env ruby
+require_relative '../lib/linternator.rb'
 
-first_file, *rest_files = ARGV 
-
-p first_file
+# folder = "tests"
+# foldder ||= ARGV 
+lint = Linternator.new("tests")
+p linterator.files
+# p first_file
 # e =  File.open("hello.txt")
 # p e.read
 
 # files = Dir["./lib/*.rb"]
 # that = File.readlines(files[0])
 # print that
+# load "bin/main.rb"
 
+# lint = Linternator.new("tests")
 # tests = "tests"
 # a = Dir["#{tests}/**/*.rb"]
 # # p 
