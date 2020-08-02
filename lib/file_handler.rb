b30936c8961256ca0fddca43850942ec51fe0f21
Vitalie Melnic <vmwhoami@gmail.com>
2020-08-02 13:54:44 +0300

    Save linter error location template in linterator.rb

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -2,13 +2,13 @@ require_relative 'term_rainbou.rb'
 class FileHandler
   def initialize(folder = 'tests')
     @folders = Dir["#{folder}/**/*.rb"]
-    self.epty_folder?
+    epty_folder?
   end
 
   def folder_query
-    h = Hash.new
+    h = {}
     @folders.each do |el|
-     h[el] = File.readlines(el)
+      h[el] = File.readlines(el)
     end
     h
   end
@@ -16,11 +16,10 @@ class FileHandler
   def error?
     @folders.empty?
   end
-   
-  def epty_folder?
-  puts "There is no test folder or it doesn't contain a '.rb' file extention".red  if self.error?
-  end
 
+  def epty_folder?
+    puts "There is no test folder or it doesn't contain a '.rb' file extention".red if error?
+  end
 end
 
 # WHAT THIS CLASS WILL DO
@@ -28,9 +27,8 @@ end
 # 2 Parse the file read though each line
 
 # load "./lib/file_handler.rb"
-files = FileHandler.new("anew")
+# files = FileHandler.new("anew")
 # tests = "tests"
 # a = Dir["#{tests}/**/*.rb"]
 # files.folder_query
 # p a
-
--- lib/linternator.rb
+++ lib/linternator.rb
@@ -1,23 +1,24 @@
+require_relative 'term_rainbou.rb'
 require_relative 'file_handler.rb'
 class Linternator
   attr_reader :files
   def initialize(folder)
     @files = FileHandler.new(folder)
-    @keywords = %w[def end do puts p print while until if unless untill]
     @llines = @files.folder_query
+    @keywords = %w[def end do puts p print while until if unless untill]
   end
 
-  def get_files
+  def error_msg; end
+
+  def allfiles
     @llines
   end
- 
-# def closing_tags(files)
-#    lieach_whith
-# end
-
 end
+
+# lib/linternator.rb:13:7: C: Naming/AccessorMethodName: Do not prefix reader method names with get_.
+#   def get_files
 
 # load "./lib/linternator.rb"
 # lint = Linternator.new("tests")
 # linter = Linternator.new("second")
-#lint.get_files
+# lint.get_files
