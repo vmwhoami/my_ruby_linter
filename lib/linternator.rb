ae81e0f1e6854948e5622877a4a80714a1f654aa
Vitalie Melnic <vmwhoami@gmail.com>
2020-08-04 00:15:15 +0300

    Add the braces_handler method

--- lib/linternator.rb
+++ lib/linternator.rb
@@ -1,11 +1,11 @@
 require_relative 'term_rainbou.rb'
 require_relative 'file_handler.rb'
 class Linternator
-  attr_reader :file , :file_name , :code_lines
+  attr_reader :file , :file_name , :code_lines, :allerrors
   def initialize(folder)
     @folders = FileHandler.new(folder)
     @file = @folders.file_query
-    @file_name = @file.keys.flatten
+    @file_name = @file.keys.flatten.join
     @code_lines =  @file.values.flatten
 		@keywords = %w[def do if unless until class case begin]
 		@allerrors = []
@@ -16,18 +16,28 @@ class Linternator
     @allerrors<<error
   end
 
+  def braces_handler
+    message = "missing closing brackets"
+    @code_lines.each_with_index do |line,indx| 
+       if self.incorect_braces?(line)
+        upadate_errors(@file_name.blue +  ": on Line #{indx+1} ".yellow + message.red)
+      end
+      
+    end
+
+  end
 
 
 
 
-  def correct_braces?(str)
+  def incorect_braces?(str)
     array = []
     braces = { '{' => '}', '[' => ']', '(' => ')' }
     str.each_char do |char|
       array << char if braces.key?(char)
-      return false if braces.key(char) && braces.key(char) != array.pop
+      return true if braces.key(char) && braces.key(char) != array.pop
     end
-    array.empty?
+    !array.empty?
   end
 
 end
@@ -38,5 +48,6 @@ end
 # load "./lib/linternator.rb"
 
 linter = Linternator.new('tests')
-p linter.correct_braces?("def ((a)")
+p linter.braces_handler
+linter.allerrors.each{|el| puts el}
 
